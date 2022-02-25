280a91248b64f0611081972caa0132db59ecfed9
dhanyaas <98949368+dhanyaas@users.noreply.github.com>
2022-02-25 17:00:09 +0530

    Add files via upload

--- pushopp.c
+++ pushopp.c
@@ -1,9 +1,10 @@
  #include<stdio.h>
 #include<stdlib.h>
-int stack[20],i,n,top=-1,num;
-void push(int n) 
+#define maxsize  2
+int stack[maxsize],i,top=-1,num;
+void push() 
 {
-	if(top==n-1)
+	if(top==maxsize-1)
 	printf("\n Stack is Full");
 	else
 	{
@@ -34,22 +35,22 @@ void display()
 	  else
 	{
 		printf("\n Stack Elements are:");
-		for(i=top;i>=0;i--)
+		for(i=0;i<=top;i++)
 		printf(" %d ",stack[i]);
 	}
 }
 void main()
 {
 	int ch;
-	printf("enter the size of stack");
-	scanf("%d",&n);
+	
 	while(1)
 	{
-		printf("\n 1.PUSH 2.POP 3.DISPLAY 4.EXIT \n Enter your choice");
+		printf("Stack operation are:");
+		printf("\n 1.PUSH\n 2.POP\n 3.DISPLAY\n 4.EXIT \n Enter your choice:");
 		scanf("%d",&ch);
 		switch(ch)	
 		{
-			case 1:push(n);
+			case 1:push();
 			break;
 			case 2:pop();
 			break;
@@ -57,7 +58,7 @@ void main()
 			break;
 			case 4: exit(0);
 			default:
-			printf("\n Invalid choic");
+			printf("\n Invalid choice");
 			break;
 		}
 	}
