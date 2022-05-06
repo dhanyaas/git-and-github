81270864797b34a7c1fe1440443897be92291b1c
Dhanyashree A <98949368+dhanyaas@users.noreply.github.com>
2022-05-06 16:36:13 +0530

    Add files via upload

--- cn2.tcl
+++ cn2.tcl
@@ -60,21 +60,23 @@ $udp0 set dst_addr_ $group
 $udp0 set dst_port_ 0
 set rcvr1 [new Agent/LossMonitor] ; #creates a receiver agent at node1(lossmonitor is agent)
 $ns attach-agent $n1 $rcvr1
-$ns at 1.9 "$n1 join-group $rcvr1 $group" ; #join the group at simulation time 1.9
+$ns at 1.10 "$n1 join-group $rcvr1 $group" ; #join the group at simulation time 1.9
+ $ns at 1.15 "$n1 leave-group $rcvr1 $group" ;
  
 set rcvr2 [new Agent/LossMonitor]; #creates a receiver agent at node2(lossmonitor is agent)
 $ns attach-agent $n2 $rcvr2
 $ns at 1.12 "$n2 join-group $rcvr2 $group" ; #join the group at simulation time 1.12
+$ns at 1.30 "$n2 leave-group $rcvr2 $group" ;
 
 set rcvr3 [new Agent/LossMonitor] ;#creates a receiver agent at node3(lossmonitor is agent)
 $ns attach-agent $n3 $rcvr3
 $ns at 1.16 "$n3 join-group $rcvr3 $group" ; #join the group at simulation time 1.15
+$ns at 1.28 "$n3 leave-group $rcvr3 $group" ; 
 
 set rcvr4 [new Agent/LossMonitor]; #creates a receiver agent at node4(lossmonitor is agent)
 $ns attach-agent $n4 $rcvr4
 $ns at 1.20 "$n4 join-group $rcvr4 $group" ; #join the group at simulation time 1.9
-
-$ns at 3.3 "$n4 leave-group $rcvr4 $group" ; #join the group at simulation time 1.9
+$ns at 1.45 "$n4 leave-group $rcvr4 $group" ; #join the group at simulation time 1.9
 #===================================
 
 #===================================
